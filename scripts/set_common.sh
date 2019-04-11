6d476d82411e2a29e14afd631fe059261fab32f4
eric-jodet <eric_jodet@fr.ibm.com>
2019-04-11 16:56:58 +0200

    add missing script
    https://github.ibm.com/org-ids/roadmap/issues/8555

---
+++ scripts/set_common.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+###############################################################################
+# Licensed Materials - Property of IBM
+# (c) Copyright IBM Corporation 2019. All Rights Reserved.
+#
+# Note to U.S. Government Users Restricted Rights:
+# Use, duplication or disclosure restricted by GSA ADP Schedule
+# Contract with IBM Corp.
+###############################################################################
+
+set -o pipefail
+
+export PATH=/opt/IBM/node-v6.7.0-linux-x64/bin:"$PATH"
